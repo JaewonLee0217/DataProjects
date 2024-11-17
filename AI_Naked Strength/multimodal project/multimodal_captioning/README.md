d1d6a86b3dff0876eb5f48f55baa1b9873c965aa
WooU <55820321+JaewonLee0217@users.noreply.github.com>
2024-11-17 17:48:53 +0900

    data_loader
    data_loader, Flickr30k class define

---
+++ AI_Naked Strength/multimodal project/multimodal_captioning/README.md
@@ -0,0 +1,10 @@
+## 24.11.17 start
+### 1. 멀티모달 이미지 캡셔닝 + 스타일 변환 (Vision + NLP)
+1. 데이터 : Flickr30k - 이미지 + 캡셔닝 포함 데이터셋
+2. 이미지 인코더 : 이미지에서의 특징 효과적 추출하기 위한 CNN 고려 예정
+3. 캡션 생성 : LSTM, Word2Vec
+4. 스타일 변환 : Attention, Transformer
+5. 이미지 생성 : GAN..
+
+* main
+
---
+++ AI_Naked Strength/multimodal project/multimodal_captioning/utils/data_loader.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+
+class Flickr30kDataset(Dataset):
+    def __init__(self, csv_file, img_dir, tranform=None):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self,idx):
+        pass
+
+def get_loader():
+    pass
