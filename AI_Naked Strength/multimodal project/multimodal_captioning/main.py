6fa77aa1a797344a0556d427e0099d26693a4b4e
WooU <55820321+JaewonLee0217@users.noreply.github.com>
2024-11-19 18:45:06 +0900

    data loader 구현
    data loader 구현

--- AI_Naked Strength/multimodal project/multimodal_captioning/main.py
+++ AI_Naked Strength/multimodal project/multimodal_captioning/main.py
@@ -1,1 +1,24 @@
-README.md
+from utils.data_loader import get_loader
+
+def main():
+    csv_file = 'TBD'
+    img_dir = 'TBD'
+
+
+    data_loader = get_loader(
+        csv_file = csv_file,
+        img_dir = img_dir,
+        batch_size = 32,
+        shuffle=True,
+        num_workers=4
+    )
+
+    # 데이터 확인
+    for images, captions in data_loader:
+        print("Image batch shape:", images.shape)
+        print("Sample caption:", captions[0])
+        break
+
+
+if __name__ == "__main__":
+    main()
--- AI_Naked Strength/multimodal project/multimodal_captioning/utils/data_loader.py
+++ AI_Naked Strength/multimodal project/multimodal_captioning/utils/data_loader.py
@@ -1,14 +1,40 @@
 import os
 import pandas as pd
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torchvision import transforms
 
 class Flickr30kDataset(Dataset):
-    def __init__(self, csv_file, img_dir, tranform=None):
-        pass
-    def __len__(self):
-        pass
-    def __getitem__(self,idx):
-        pass
+    # csv_file에서 annotations, img 주소, 형태를 정의
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
 
-def get_loader():
-    pass
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self,idx):
+        img_name = os.path.join(self.img_dir,self.annotations.iloc[idx, 0])
+        image = Image.open(img_name).convert('RGB') #image열어서 RGB로 변형
+        caption = self.annotations.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+
+        return image, caption
+
+def get_loader(csv_file, img_dir, batch_size, shuffle, num_workers):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+    dataset = Flickr30kDataset(csv_file, img_dir, transform=transform)
+
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers
+    )
+    return data_loader
