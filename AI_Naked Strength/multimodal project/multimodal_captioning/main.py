fa10cd1391e72de178b667856543190ab6043d06
WooU <55820321+JaewonLee0217@users.noreply.github.com>
2024-11-26 03:29:09 +0900

    fix path error
    revised path and requirements

--- AI_Naked Strength/multimodal project/multimodal_captioning/main.py
+++ AI_Naked Strength/multimodal project/multimodal_captioning/main.py
@@ -1,12 +1,12 @@
 from utils.data_loader import get_loader
 
 def main():
-    img_dir = 'AI_Naked Strength/multimodal project/data/flickr30k/images'
-    caption_file = 'AI_Naked Strength/multimodal project/data/flickr30k/captions.txt'
+    img_dir = './data/flickr30k/Images'
+    caption_file = './data/flickr30k/captions.txt'
 
     data_loader = get_loader(
         img_dir = img_dir,
-        captino_file=caption_file,
+        caption_file=caption_file,
         batch_size = 32,
         shuffle=True,
         num_workers=4
