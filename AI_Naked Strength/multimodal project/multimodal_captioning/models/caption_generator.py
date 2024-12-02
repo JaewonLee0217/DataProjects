88ed8873cf32e0d8908381353b7797b7c0da69a5
WooU <55820321+JaewonLee0217@users.noreply.github.com>
2024-12-03 02:37:08 +0900

    Update caption_generator.py
    입력 캡션을 임베딩-> 이미지 특징과 임베딩된 캡션을 연결 -> LSTM을 통해 시퀀스를 처리 -> 선형 층으로 각 시점의 출력을 생성

--- AI_Naked Strength/multimodal project/multimodal_captioning/models/caption_generator.py
+++ AI_Naked Strength/multimodal project/multimodal_captioning/models/caption_generator.py
@@ -6,9 +6,29 @@ import torchvision.models as models
 # 추출한 특징을 입력으로 받아 이미지에 대한 캡션을 생성.
 
 class CaptionGenerator(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        pass
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(CaptionGenerator, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, features, captions):
-        pass
+        embeddings = self.dropout(self.embed(captions))
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
+
+    def sample(self, features, states=None, max_len=20):
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        sampled_ids = torch.stack(sampled_ids, 1)
+        return sampled_ids
